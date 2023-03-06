89ec80b4ca6d01160397f06b8a479691f2e9576c
DutchEllie <personal@quenten.nl>
2023-03-06 11:16:23 +0100

    Finish JS refactor

--- static/components/searchpage.js
+++ static/components/searchpage.js
@@ -24,14 +24,12 @@ export class SearchPage extends HTMLElement {
 		return ['data-searchquery'];
 	}
 
-	async update() {
-		return getSearchResults(this.searchQuery);
-
-	}
-
 	attributeChangedCallback(prop, oldVal, newVal) {
 		if (prop === 'data-searchquery') {
 			this.searchQuery = newVal;
+
+			// Just like on artpage, calling render here makes it reactive
+			// not that we use it, but why not add it anyway?
 			this.render();
 		}
 	}
@@ -43,6 +41,7 @@ export class SearchPage extends HTMLElement {
 	async render() {
 		this.innerHTML = "";
 
+		// Did you know that you can't edit the DOM in the constructor?
 		this.appendChild(this.searchResultsTitle)
 		this.appendChild(this.closer);
 		try {
@@ -53,18 +52,15 @@ export class SearchPage extends HTMLElement {
 				artPieceResult.id = piece.objectNumber;
 				artPieceResult.setAttribute('src', piece.webImage.url);
 				artPieceResult.returnPath = window.location.hash;
-				// artPieceResult.id = piece.artObject.objectNumber;
 				this.appendChild(artPieceResult);
 			})
 		} catch(e) {
 			console.error(e);
 			for (let i = 0; i < 20; i++) {
 				const artPieceResult = document.createElement('quickart-element')
-				// artPieceResult.data = ;
 				artPieceResult.id = 'error';
 				artPieceResult.setAttribute('src', '/static/error.jpg');
 				artPieceResult.returnPath = window.location.hash;
-				// artPieceResult.id = piece.artObject.objectNumber;
 				this.appendChild(artPieceResult);
 			}
 		}
